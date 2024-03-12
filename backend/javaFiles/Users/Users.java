c665a32bdd4efd5eb0154b0370cceda859788d7b
Ashx11 <92754282+Ashx11@users.noreply.github.com>
2024-03-12 12:54:30 -0400

    Update Users.java

--- backend/javaFiles/Users/Users.java
+++ backend/javaFiles/Users/Users.java
@@ -1,5 +1,15 @@
 package backend.javaFiles.Users;
 
-public class Users {
-    
+public abstract class User {
+    private String username;
+
+    public User(String username) {
+        this.username = username;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public abstract String getUserType();
 }
