f39657fb1a9ce80e37eaec0296fd206363ff63d7
Ashx11 <92754282+Ashx11@users.noreply.github.com>
2024-03-13 00:29:52 -0400

    Update Guest.java

--- backend/javaFiles/Users/Guest.java
+++ backend/javaFiles/Users/Guest.java
@@ -3,6 +3,8 @@ package backend.javaFiles.Users;
 class Guest extends User {
     public Guest(String username) {
         super(username);
+        permissions.add(Permission.TURN_ON_OFF_LIGHTS);
+        permissions.add(Permission.OPEN_CLOSE_WINDOWS);
     }
 
     @Override
