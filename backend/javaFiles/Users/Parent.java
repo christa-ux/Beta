a2474d22bfeb570699470a4c27f3141a6a7dfc5b
Ashx11 <92754282+Ashx11@users.noreply.github.com>
2024-03-13 00:29:21 -0400

    Update Parent.java

--- backend/javaFiles/Users/Parent.java
+++ backend/javaFiles/Users/Parent.java
@@ -1,1 +1,12 @@
 package backend.javaFiles.Users;
+class Parent extends User {
+    public Parent(String username) {
+        super(username);
+        permissions = EnumSet.allOf(Permission.class); // all permissons for parents
+    }
+
+    @Override
+    public String getUserType() {
+        return "Parent";
+    }
+}
