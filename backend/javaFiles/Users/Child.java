c027981c5f765c4ddbf431d7399105c49726a378
Ashx11 <92754282+Ashx11@users.noreply.github.com>
2024-03-13 00:28:22 -0400

    Update Child.java

--- backend/javaFiles/Users/Child.java
+++ backend/javaFiles/Users/Child.java
@@ -3,6 +3,8 @@ package backend.javaFiles.Users;
 class Child extends User {
     public Child(String username) {
         super(username);
+        permissions.add(Permission.TURN_ON_OFF_LIGHTS);
+        permissions.add(Permission.OPEN_CLOSE_WINDOWS);
     }
 
     @Override
